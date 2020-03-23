3d44d377c1688e21e22962458afdcf1c7769f532
Christopher Rigby <christopher.rigby@onthebeach.co.uk>
2020-03-23 22:25:18 +0000

    Initial commit

---
+++ openvpnas_backup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+backup_date=$(date +%Y%m%d%H%M)
+openvpnas_base=/usr/local/openvpn_as
+source_dir=${openvpnas_base}/etc/db
+backup_dir=/data/backup/openvpnas/${backup_date}
+openvpnas_dbs=(
+  config.db
+  certs.db
+  userprop.db
+  log.db
+  config_local.db
+  cluster.db
+  clusterdb.db
+  notification.db
+)
+
+[ ! -d $backup_dir ] && mkdir -p $backup_dir
+
+function log() {
+  content=$(echo "$(date +%F' '%H:%M:%S) $0 $1")
+  logger -t openvpn_backup $content
+  echo $content
+}
+
+function backup_openvpnas_db() {
+  log "${FUNCNAME[0]}: Backing up ${db}"
+  [ -e $source_dir/$1 ] && sqlite3 $source_dir/$1 .dump > $backup_dir/$1.bak
+  [ $? -eq 0 ] && log "${FUNCNAME[0]}: Done" || log "${FUNCNAME[0]}: Failed"
+}
+
+function backup_openvpnas_configs() {
+  log "${FUNCNAME[0]}: Backing up running configuration: "
+  $openvpnas_base/scripts/sacli ConfigQuery > $backup_dir/config.json
+  [ $? -eq 0 ] && log "${FUNCNAME[0]}: Done" || log "${FUNCNAME[0]}: Failed"
+  log "${FUNCNAME[0]}: Backing up static configuration: "
+  cp {$openvpnas_base/etc,$backup_dir}/as.conf
+  [ $? -eq 0 ] && log "${FUNCNAME[0]}: Done" || log "${FUNCNAME[0]}: Failed"
+}
+
+function symlink_last_backup() {
+  log "${FUNCNAME[0]}: Creating 'current' symlink: "
+  [ -L ${backup_dir}/current ] && unlink $(dirname ${backup_dir})/current
+  ln -s ${backup_dir} $(dirname ${backup_dir})/current
+  [ $? -eq 0 ] && log "${FUNCNAME[0]}: Done" || log "${FUNCNAME[0]}: Failed"
+}
+
+for db in ${openvpnas_dbs[@]}
+do
+  backup_openvpnas_db $db
+done
+backup_openvpnas_configs
+symlink_last_backup
